fc6a8d6b0702d25c33f3414059a02995fa45e483
Your Name <you@example.com>
2022-07-24 19:41:55 -0400

    Define model associations

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+  //({force: true}) causes database connection to sync with model definitions and association, overwrites tables and re-creates them with association changes
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
